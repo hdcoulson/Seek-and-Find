f4c9a29726e51a454bfb863204c18e9d352a4acd
Harry Coulson <coulson.mail@me.com>
2017-05-10 14:13:27 -0700

    User can view game title #2

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $("#level-1-button").click(function() {
     $("#landing-page").addClass("hidden")
     $("#shapes-game-view").removeClass("hidden")
+  })
+  $(document).ready(function() {
+    $("#back-button").click(function() {
+      $("#shapes-game-view").addClass("hidden")
+      $("#landing-page").removeClass("hidden")
+    })
   })
 })
