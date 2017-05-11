fa3272fdabd3f473ca07c1bbaed2d9b1eea90eed
Harry Coulson <coulson.mail@me.com>
2017-05-11 15:12:58 -0700

    Provide user with feedback on shape selection #2

--- listeners.js
+++ listeners.js
@@ -3,15 +3,30 @@ $(document).ready(function() {
     $("#landing-page").addClass("hidden")
     $("#shapes-game-view").removeClass("hidden")
   })
-  $(document).ready(function() {
-    $("#back-button").click(function() {
-      $("#shapes-game-view").addClass("hidden")
-      $("#landing-page").removeClass("hidden")
-    })
+})
+
+$(document).ready(function() {
+  $("#back-button").click(function() {
+    $("#shapes-game-view").addClass("hidden")
+    $("#landing-page").removeClass("hidden")
   })
-  $(document).ready(function() {
-    $(".shape").click(function() {
-      $(this).toggle("fade")
-    })
-  })
+})
+
+document.addEventListener('click', function(event) {
+  const $targetImage = document.querySelector('#target-image').name
+  if (event.target.name !== $targetImage) {
+    if (event.target.classList.value === 'shape') {
+      event.target.classList.add('shake-chunk')
+    }
+    else {
+      return
+    }
+  }
+  else if (event.target.name === $targetImage) {
+    if (event.target.id !== 'target-image') {
+    event.target.classList.add('hidden')
+    }
+    //add more animations and add to score
+  }
+
 })
