22015d8009f3df075279d202d41ec1c808cf1904
Harry Coulson <coulson.mail@me.com>
2017-05-18 09:47:27 -0700

    Removed jQuery listener on logo #4

--- listeners.js
+++ listeners.js
@@ -1,21 +1,13 @@
-$(document).ready(function() {
-  $('#logo').click(function() {
-    $('#game-view').addClass('hidden')
-    $('#homepage').removeClass('hidden')
-  })
-})
-
 document.addEventListener('click', function(event) {
   const $sampleImage = document.querySelector('#sample-image')
-  const $homepage = document.querySelector('#homepage')
-  const $gameView = document.querySelector('#game-view')
+  const $homepage = document.querySelector('#homepage')//
+  const $gameView = document.querySelector('#game-view')//
   const $play = document.querySelector('#play')
-  const $logo = document.querySelector('#logo')
   const $matches = event.target.name
-  const $shapes = game.shapes
+  const $shapes = game.shapes//
   const $index = indexPosition($shapes, event.target.id)
-  const $instructions = document.querySelector('#instructions')
-  const $sampleShape = document.querySelector('#sample-shape')
+  const $instructions = document.querySelector('#instructions')//
+  const $sampleShape = document.querySelector('#sample-shape')//
 
   if (event.target === $play) {
     $homepage.classList.add('hidden')
@@ -28,6 +20,7 @@ document.addEventListener('click', function(event) {
     event.target.classList.add('shake-chunk')
   }
   else if (event.target.id !== 'sample-image' && event.target.tagName === 'IMG') {
+    console.log(test($shapes))
     event.target.classList.add('hidden')
     $shapes[$index].found = true
 
